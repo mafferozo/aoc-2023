9633dec1117e0607c97b9527bc5f88adb51f73b5
mafferozo <martijn.v.erne@gmail.com>
2023-12-18 17:42:11 +0100

    day-1: add aoc_input lib

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+use anyhow::{Context, Result};
+use aoc_input_lib::get_puzzle_input;
+fn main() -> Result<()> {
     println!("Hello, world!");
+    let input = get_puzzle_input(2023, 1, None).context("Could not retrieve puzzle input")?;
+    println!("{}", input);
+    Ok(())
 }
