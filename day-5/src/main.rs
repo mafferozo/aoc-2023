5406cf57a0dee744961b9a377dd3989db4267949
mafferozo <martijn.v.erne@gmail.com>
2023-12-18 17:42:11 +0100

    day-5: star 1

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -26,6 +26,68 @@ fn main() -> Result<()> {
     } else {
         get_puzzle_input(2023, 5, session).context("Could not retrieve puzzle input")?
     };
-    println!("{}", input);
+
+    let re = Regex::new(r"seeds: (.*)\n")?;
+
+    let (_full_match, [seeds_match]) = re
+        .captures(&input)
+        .context("puzzle should contain seeds array!")?
+        .extract();
+
+    let mut seeds = vec![];
+    for seed in seeds_match.split_ascii_whitespace() {
+        let seed = u64::from_str_radix(seed, 10).context("Seeds can only be numbers!")?;
+        seeds.push(seed);
+    }
+
+    let maps: Vec<Vec<(u64, u64, u64)>> = [
+        "seed-to-soil",
+        "soil-to-fertilizer",
+        "fertilizer-to-water",
+        "water-to-light",
+        "light-to-temperature",
+        "temperature-to-humidity",
+        "humidity-to-location",
+    ]
+    .iter()
+    .map(|map_name| parse_map(&input, *map_name))
+    .collect::<Result<Vec<_>>>()?;
+
+    let mut location = u64::MAX;
+    dbg!(&seeds);
+    for seed in seeds.iter() {
+        let mut next = *seed;
+        for map in maps.iter() {
+            for (dest,source,len) in map.iter() {
+                if (*source..(source+len)).contains(&next) {
+                    // println!("contains next: {}, dest: {}, source: {}, len: {}",next,dest,source,len);
+                    next = dest + (next - source);
+                    break;
+                }
+            }
+        }
+        location = location.min(next);
+    }
+    println!("part one: {}",location);
     Ok(())
+}
+
+fn parse_map(input: &str, start_of_map: &str) -> Result<Vec<(u64, u64, u64)>, anyhow::Error> {
+    let mut nums = input
+        .split_ascii_whitespace()
+        // skip until map name
+        .skip_while(|word| *word != start_of_map)
+        // skip map name and "map:" word
+        .skip(2)
+        // take every number
+        .take_while(|word| word.chars().all(|ch| ch.is_ascii_digit()));
+
+    let mut map = vec![];
+    while let Some(dest) = nums.next() {
+        let dest = u64::from_str_radix(dest, 10)?;
+        let source = u64::from_str_radix(nums.next().unwrap(), 10)?;
+        let len = u64::from_str_radix(nums.next().unwrap(), 10)?;
+        map.push((dest, source, len))
+    }
+    Ok(map)
 }
