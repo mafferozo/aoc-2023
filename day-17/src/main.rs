c8f450ff7754a98021e08be21b3359214ab5ec63
Martijn van Erne <martijn.van.erne@mn.nl>
2024-01-01 19:35:59 +0100

    wip: day-17

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,4 +1,8 @@
-use std::{collections::{HashMap, HashSet}, env};
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashSet},
+    env,
+};
 
 use anyhow::{Context, Result};
 use aoc_input_lib::get_puzzle_input;
@@ -26,41 +30,85 @@ fn main() -> Result<()> {
     } else {
         get_puzzle_input(2023, 17, session).context("Could not retrieve puzzle input")?
     };
-    println!("{input}");
-
     let g = Grid::parse_grid(&input);
 
-    let mut stack = vec![(0, 0, Dirs::new(), 0)];
-    // let mut visited: HashMap<(usize, usize), u32> = HashMap::new();
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
-
-    while let Some((x, y, past_dirs, cost)) = stack.pop() {
-        // todo: check past_dirs
-        // let saved_cost = visited.entry((x, y)).or_insert(u32::MAX);
-        // if *saved_cost <= cost {
-        //     continue;
-        // }
-        // *saved_cost = cost;
-        if !visited.insert((x,y)) {
-            continue
-        }
-        for to_dir in Dirs::possible_dirs(&g, past_dirs, (x, y)) {
-            let (x,y) = to_dir.get_neighbour_unchecked((x,y));
-            let cost = cost + g.data[y][x];
-            stack.push((x, y, past_dirs.append(*to_dir), cost));
-        }
-    }
-    assert!(visited.len() == g.rows * g.cols);
-    dbg!(visited.get(&(g.cols - 1, g.rows - 1)));
+    let part_one = dijkstra_part_one(&g);
+    println!("part one: {}", part_one);
 
     Ok(())
+}
+
+fn dijkstra_part_one(g: &Grid) -> u32 {
+    // initial state
+    let mut queue = BinaryHeap::from(vec![
+        State::new(0, Position::new((0, 0), Direction::Down), 1),
+        State::new(0, Position::new((0, 0), Direction::Right), 1),
+    ]);
+
+    // set of visited nodes
+    let mut visited: HashSet<Position> = HashSet::new();
+
+    // target location
+    let target = (g.cols - 1, g.rows - 1);
+
+    let mut heat = 0;
+
+    while let Some(state) = queue.pop() {
+        let loc = (state.position.x, state.position.y);
+
+        // if we arrived at the target location, return the answer
+        if loc == target {
+            heat = state.heat;
+            break;
+        }
+
+        // if we've already visited this node, we visited this node with a better heat value.
+        // So skip this one.
+        if !visited.insert(state.position) {
+            continue;
+        }
+
+        // we can always rotate and move clock-wise
+        let dir = state.position.dir.rotate_clockwise();
+        if let Some(new_loc) = g.move_along_dir(loc, &dir) {
+            queue.push(State::new(
+                state.heat + g.get(new_loc),
+                Position::new(new_loc, dir),
+                1,
+            ))
+        }
+
+        // we can always rotate and move counter-clock-wise
+        let dir = state.position.dir.rotate_counter_clockwise();
+        if let Some(new_loc) = g.move_along_dir(loc, &dir) {
+            queue.push(State::new(
+                state.heat + g.get(new_loc),
+                Position::new(new_loc, dir),
+                1,
+            ))
+        }
+
+        // we've already moved three times in this direction
+        if state.steps > 3 {
+            continue;
+        }
+        let dir = state.position.dir;
+        if let Some(new_loc) = g.move_along_dir(loc, &dir) {
+            queue.push(State::new(
+                state.heat + g.get(new_loc),
+                Position::new(new_loc, dir),
+                state.steps + 1,
+            ))
+        }
+    }
+    return heat;
 }
 
 #[derive(Debug, Clone)]
 struct Grid {
     data: Vec<Vec<u32>>,
-    cols: usize,
-    rows: usize,
+    cols: isize,
+    rows: isize,
 }
 
 impl Grid {
@@ -78,24 +126,18 @@ impl Grid {
 
         // sanity check: all values are parsed correctly
         assert!(data.iter().flat_map(|row| row.iter()).all(|x| *x < 10));
-        Self { data, cols, rows }
-    }
 
-    fn possible_dirs(&self, pos: (usize, usize)) -> impl Iterator<Item = &Direction> {
-        ALL_DIRS.iter().filter(move |d| self.is_in_grid(pos, d))
-    }
-
-    fn is_in_grid(&self, (x, y): (usize, usize), dir: &Direction) -> bool {
-        use Direction::*;
-        match dir {
-            Up => y > 0,
-            Down => y + 1 < self.rows,
-            Left => x > 0,
-            Right => x + 1 < self.cols,
+        // number of cols, number of rows should fit in a isize
+        Self {
+            data,
+            cols: isize::try_from(cols).unwrap(),
+            rows: isize::try_from(rows).unwrap(),
         }
     }
 
-    fn clamp(&self, (x, y): (usize, usize), dir: Direction) -> Option<(usize, usize)> {
+    /// Move the current position `(x,y)` along Direction `dir`
+    /// Returns `Some(x_new,y_new)` if the move is valid; i.e. the new position lies in the grid.
+    fn move_along_dir(&self, (x, y): (isize, isize), dir: &Direction) -> Option<(isize, isize)> {
         use Direction::*;
         assert!(x < self.cols);
         assert!(y < self.rows);
@@ -107,99 +149,88 @@ impl Grid {
             Down => (y + 1 < self.rows).then(|| (x, y + 1)),
         }
     }
+
+    fn get(&self, (x, y): (isize, isize)) -> u32 {
+        self.data[y as usize][x as usize]
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum Direction {
     Up,
+    Right,
     Down,
     Left,
-    Right,
 }
-
-const ALL_DIRS: &[Direction; 4] = &[
-    Direction::Up,
-    Direction::Down,
-    Direction::Left,
-    Direction::Right,
-];
 
 impl Direction {
-    fn possible_dirs(&self) -> [Self; 3] {
+    fn rotate_clockwise(&self) -> Self {
         use Direction::*;
         match self {
-            Up => [Up, Left, Right],
-            Down => [Down, Left, Right],
-            Left => [Up, Down, Left],
-            Right => [Up, Down, Right],
+            Up => Left,
+            Left => Down,
+            Down => Right,
+            Right => Up,
         }
     }
 
-    fn opposite_dir(&self) -> Self {
+    fn rotate_counter_clockwise(&self) -> Self {
         use Direction::*;
         match self {
-            Up => Down,
-            Down => Up,
-            Left => Right,
-            Right => Left,
-        }
-    }
-
-    fn get_neighbour_unchecked(&self, (x, y): (usize, usize)) -> (usize,usize) {
-        use Direction::*;
-        match self {
-            Up => (x, y - 1),
-            Down => (x, y + 1),
-            Left => (x - 1, y),
-            Right => (x + 1, y),
+            Up => Right,
+            Left => Up,
+            Down => Left,
+            Right => Down,
         }
     }
 }
 
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-/// Keep track of the last three directions of a path, if there are any
-struct Dirs([Option<Direction>; 3]);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Position {
+    x: isize,
+    y: isize,
+    dir: Direction,
+}
 
-impl Dirs {
-    fn new() -> Self {
-        Self([None, None, None])
+impl Position {
+    fn new((x, y): (isize, isize), dir: Direction) -> Self {
+        Self { x, y, dir }
     }
+}
 
-    // Append a new direction to the log
-    fn append(&self, dir: Direction) -> Self {
-        let new = Some(dir);
+/// Represents a state of a single path the Crucible can walk
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct State {
+    /// The accumulated heat of the path
+    heat: u32,
+    /// The current position
+    position: Position,
+    /// The current amount of steps in the same direction
+    steps: usize,
+}
 
-        // rotate past dirs
-        Self([new, self.0[0], self.0[1]])
-    }
-
-    // Returns the last direction
-    fn last(&self) -> Option<Direction> {
-        self.0[0]
-    }
-
-    /// Returns true if the past three directions are equal
-    fn is_same(&self) -> bool {
-        match (self.0[0], self.0[1], self.0[2]) {
-            (Some(v1), Some(v2), Some(v3)) => v1 == v2 && v1 == v3,
-            _ => false,
+impl State {
+    fn new(heat: u32, position: Position, steps: usize) -> Self {
+        Self {
+            heat,
+            position,
+            steps,
         }
     }
+}
 
-    /// Returns the possible directions of a location in the grid, filtered by:
-    /// - their opposite direction (we can not go back)
-    /// - the forward direction, if we already went forward three times
-    fn possible_dirs(
-        grid: &Grid,
-        past_dirs: Dirs,
-        pos: (usize, usize),
-    ) -> impl Iterator<Item = &Direction> {
-        grid.possible_dirs(pos).filter(move |d| {
-            let last_dir = past_dirs.0[0];
-            last_dir.is_none()
-                || last_dir.is_some_and(|last_dir| {
-                    last_dir != d.opposite_dir() || (**d == last_dir && !past_dirs.is_same())
-                })
-        })
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .heat
+            .cmp(&self.heat)
+            .then_with(|| self.position.cmp(&other.position))
+            .then_with(|| other.steps.cmp(&self.steps))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
