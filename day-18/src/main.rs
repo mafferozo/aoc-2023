6edcd0dd30e4a139f2f661b54dc7587f7ac50ec4
Martijn van Erne <martijn.van.erne@mn.nl>
2024-01-02 20:56:06 +0100

    feat: day-18

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -1,11 +1,9 @@
 use std::env;
 
-use anyhow::{anyhow, Context, Result};
+use ::phf::{phf_map, Map};
+use anyhow::{Context, Result};
 use aoc_input_lib::get_puzzle_input;
 use clap::Parser;
-
-use regex::Regex;
-use Direction::*;
 
 /// Lavaduct Lagoon
 #[derive(Parser, Debug)]
@@ -20,6 +18,18 @@ struct Args {
     session: Option<String>,
 }
 
+static MAP: Map<&'static str, (i64, i64)> = phf_map! {
+    "R" => (1,0),
+    "D" => (0,1),
+    "L" => (-1,0),
+    "U" => (0,-1),
+
+    "0" => (1,0),
+    "1" => (0,1),
+    "2" => (-1,0),
+    "3" => (0,-1),
+};
+
 fn main() -> Result<()> {
     let args = Args::parse();
     let session = args.session.or_else(|| env::var("SESSION").ok());
@@ -29,58 +39,61 @@ fn main() -> Result<()> {
     } else {
         get_puzzle_input(2023, 18, session).context("Could not retrieve puzzle input")?
     };
-    println!("{input}");
-    let instructions = DigInstruction::parse_input(&input)?;
-    dbg!(instructions);
+
+    println!("{}", part_one(&input));
+    println!("{}", part_two(&input));
     Ok(())
 }
 
-#[derive(Debug, Clone, Copy)]
-struct DigInstruction {
-    /// The direction to dig in
-    direction: Direction,
-
-    /// The trench length in meters
-    trench_length: u32,
-
-    /// color of the trench 0x00RRGGBB
-    trench_color: u32,
+fn part_one(input: &str) -> i64 {
+    let iter = input.lines().map(|l| {
+        let mut it = l.split_ascii_whitespace();
+        let (x, y) = MAP.get(it.next().unwrap()).unwrap();
+        let n = i64::from_str_radix(it.next().unwrap(), 10).unwrap();
+        (*x, *y, n)
+    });
+    f(iter)
 }
 
-impl DigInstruction {
-    fn parse_input(input: &str) -> Result<Vec<Self>> {
-        let re = Regex::new(r"^(\w) (\d+) \(#(.*)\)$").unwrap();
+fn part_two(input: &str) -> i64 {
+    let iter = input.lines().map(|l| {
+        let color = l.split_ascii_whitespace().skip(2).next().unwrap();
 
-        let mut instructions = vec![];
-        for line in input.lines() {
-            let (_m, [dir, length, color]) = re.captures(line).context("should match")?.extract();
-            instructions.push(DigInstruction {
-                direction: Direction::parse(dir)?,
-                trench_length: u32::from_str_radix(length, 10)?,
-                trench_color: u32::from_str_radix(color, 16)?,
-            });
-        }
+        let n = i64::from_str_radix(&color[2..7], 16).unwrap();
+        let (x, y) = MAP.get(&color[7..8]).unwrap();
+        (*x, *y, n)
+    });
+    f(iter)
+}
 
-        Ok(instructions)
+fn f(steps: impl Iterator<Item = (i64, i64, i64)>) -> i64 {
+    // first point is at (0,0)
+    let (mut pos_x, mut pos_y) = (0, 0);
+
+    let mut boundary_points = 0;
+    let mut area = 0;
+
+    for (x, y, n) in steps {
+        // position of new point
+        let new_pos_x = pos_x + x * n;
+        let new_pos_y = pos_y + y * n;
+
+        // computes area of triangle
+        area += pos_x * new_pos_y - new_pos_x * pos_y;
+
+        // count all the boundary points too
+        boundary_points += n;
+
+        // update position of old point
+        pos_x = new_pos_x;
+        pos_y = new_pos_y;
     }
-}
+    // note: we still need to divide the area by two after the integral
+    let area = area / 2;
+    // Picks' theorem
+    // area = i + b/2 -1
+    // i = area - b/2 + 1
+    let inside_points = area - boundary_points / 2 + 1;
 
-#[derive(Debug, Clone, Copy)]
-enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-
-impl Direction {
-    fn parse(m: &str) -> Result<Self> {
-        match m {
-            "U" => Ok(Up),
-            "R" => Ok(Right),
-            "D" => Ok(Left),
-            "L" => Ok(Down),
-            _ => Err(anyhow!("Should match")),
-        }
-    }
+    inside_points + boundary_points
 }
